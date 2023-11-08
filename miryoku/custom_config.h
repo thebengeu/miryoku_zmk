0ebb704a416530c89e4c923bb371a29ff6fb42df
Beng Eu <beng@bengeu.com>
2023-11-08 17:22:17 +0800

    Switch tab and esc and enable kludge thumbcombos

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,6 +3,7 @@
 
 #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_CLIPBOARD_WIN
+#define MIRYOKU_KLUDGE_THUMBCOMBOS
 #define MIRYOKU_LAYERS_FLIP
 #define MIRYOKU_NAV_INVERTEDT
 #define MIRYOKU_TAP_QWERTY
@@ -11,7 +12,7 @@
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_NUM, BSPC), U_LT(U_SYM, RET), U_LT(U_FUN, DEL),   U_LT(U_MEDIA, TAB),U_LT(U_MOUSE, ESC),U_LT(U_NAV, SPACE),U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_NUM, BSPC), U_LT(U_SYM, RET), U_LT(U_FUN, DEL),   U_LT(U_MEDIA, ESC),U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NAV \
 &kp PG_UP,         &kp HOME,          &kp UP,            &kp END,           &kp INS,           U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
