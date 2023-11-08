3a16c2bf5ccd49c6e27348401542a7cc0940ac98
Beng Eu <beng@bengeu.com>
2023-11-08 17:27:48 +0800

    Switch esc to tab in function layer too

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -36,4 +36,4 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp PSCRN,         &kp F7,            &kp F8,            &kp F9,            &kp F12,           \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp SLCK,          &kp F4,            &kp F5,            &kp F6,            &kp F11,           \
 U_NA,              &kp RALT,          &u_to_U_MEDIA,     &u_to_U_FUN,       U_NA,              &kp PAUSE_BREAK,   &kp F1,            &kp F2,            &kp F3,            &kp F10,           \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp K_APP,         &kp ESC,           &kp SPACE,         U_NP,              U_NP
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp K_APP,         &kp TAB,           &kp SPACE,         U_NP,              U_NP
